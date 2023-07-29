e2cbcbdb75beec6b5889c63ddea3f27077b271d8
Vlada-WoodyChick <uchusa@mail.ru>
2023-07-29 22:33:41 +0300

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
 // useRef - возвращает изменяемый ref-объект, свойство .current которого инициализируется переданным аргументом (initialValue). Возвращённый объект будет сохраняться в течение всего времени жизни компонента.
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import './App.css';
-
+const complexCompute =(num) => {
+  let i = 0;
+  while (i < 1000000000) i++
+  return num * 2;
+}
 function App() {
- // const [renderCount, setRenderCount] = useState(1);
-  const [value, setValue] =useState('initial');
-  const renderCount = useRef(1);
-  const inputRef = useRef(null);
-  const focus = () => inputRef.current.focus();
-  const prevValue = useRef('');
-  useEffect(() => {
-    renderCount.current ++;  
-    console.log(inputRef.current.value);  
-  });
-  useEffect( () => {
-    prevValue.current = value;
-  }, [value])
+ const [number, setNumber] = useState(42);
+ 
+const addNumber =() => setNumber(number => number + 1);
+const substractNumber =() => {  
+  setNumber(number => number -1);
+}
+const[colored, setColored] = useState(false);
+const styles = useMemo( () => (  {
+  color: colored ? 'darkred' : 'black'
+ } ), [colored])
+useEffect( () => console.log('styles changed'), [styles])
 
+const computed = useMemo(
+  ()=> complexCompute(number), [number]
+) 
  return(
     <div>
-<h1>RenderCount: {renderCount.current}</h1>
-<h2>Previous value: {prevValue.current}</h2>
-<input ref={inputRef} type="text" onChange={ e =>setValue(e.target.value)  } value={value}/>
-<button className='btn btn-success' onClick={focus}>focus</button>
+<h1 style = {styles}>Computed property: {computed}</h1>
+<button className="btn btn-success" onClick= {addNumber} >ADD</button>
+<button className="btn btn-danger" onClick={substractNumber }>SUBTRACT</button>
+<button className="btn btn-warning" onClick={ () => setColored(colored => !colored)}>CHANGE</button>
 
     </div>
   )
