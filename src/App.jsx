35dd54ab12dbc4438ea258cf335e4c5e68f93b26
Vlada-WoodyChick <uchusa@mail.ru>
2023-07-29 23:19:18 +0300

    useCallback feature1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 // useRef - возвращает изменяемый ref-объект, свойство .current которого инициализируется переданным аргументом (initialValue). Возвращённый объект будет сохраняться в течение всего времени жизни компонента.
 
-import React, { useState, useEffect, useRef, useMemo } from 'react';
+import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import './App.css';
-const complexCompute =(num) => {
-  let i = 0;
-  while (i < 1000000000) i++
-  return num * 2;
-}
+
 function App() {
- const [number, setNumber] = useState(42);
- 
-const addNumber =() => setNumber(number => number + 1);
-const substractNumber =() => {  
-  setNumber(number => number -1);
-}
-const[colored, setColored] = useState(false);
-const styles = useMemo( () => (  {
+const [colored, setColored] = useState(false);
+const [count, setCount] = useState(1);
+const styles = {
   color: colored ? 'darkred' : 'black'
- } ), [colored])
-useEffect( () => console.log('styles changed'), [styles])
+}
+const colorButtonHandler =() => {
+  setColored(colored => !colored);
+}
+const countButtonHandler =() => {
+  setCount(count => count + 1);
+}
+useEffect (colorButtonHandler, [count]);
 
-const computed = useMemo(
-  ()=> complexCompute(number), [number]
-) 
- return(
+
+return(
     <div>
-<h1 style = {styles}>Computed property: {computed}</h1>
-<button className="btn btn-success" onClick= {addNumber} >ADD</button>
-<button className="btn btn-danger" onClick={substractNumber }>SUBTRACT</button>
-<button className="btn btn-warning" onClick={ () => setColored(colored => !colored)}>CHANGE</button>
-
+<h1 style={styles}>Number of elements {count}</h1>
+<button className="btn btn-success" onClick={countButtonHandler}></button>
+<button className="btn btn-danger" onClick={colorButtonHandler}></button>
     </div>
   )
 }
