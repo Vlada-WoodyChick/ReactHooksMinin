1289782fe14d674d41ea5636e7d88df0ca9e0d78
Vlada-WoodyChick <uchusa@mail.ru>
2023-07-30 13:18:03 +0300

    useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // useRef - возвращает изменяемый ref-объект, свойство .current которого инициализируется переданным аргументом (initialValue). Возвращённый объект будет сохраняться в течение всего времени жизни компонента.
 
 import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react';
+import {ItemsList} from "./ItemsList";
 import './App.css';
 
 function App() {
@@ -15,14 +16,18 @@ const colorButtonHandler =() => {
 const countButtonHandler =() => {
   setCount(count => count + 1);
 }
-useEffect (colorButtonHandler, [count]);
-
+//useEffect (colorButtonHandler, [count]);
+const generateItemsFromAPI = useCallback( (indexNumber) => {
+  return new Array(count).fill('').map( (_, i) => `element ${i + indexNumber}` )
+}, [count] )
+ 
 
 return(
     <div>
 <h1 style={styles}>Number of elements {count}</h1>
-<button className="btn btn-success" onClick={countButtonHandler}></button>
-<button className="btn btn-danger" onClick={colorButtonHandler}></button>
+<button className="btn btn-success" onClick={countButtonHandler}>ADD</button>
+<button className="btn btn-warning" onClick={colorButtonHandler}>COLOR</button>
+<ItemsList getItems={generateItemsFromAPI}/>
     </div>
   )
 }
